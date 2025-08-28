870bc2b58fd092671b92f42c02382e1047e5a8e5
Thomas Seropian <thomas@seropian.io>
2025-08-28 11:53:37 +0100

    fix: MySQL credentials

--- server/config/config.js
+++ server/config/config.js
@@ -7,12 +7,40 @@ export default {
     dialect: process.env.DB_DIALECT || 'mysql',
     port: process.env.DB_PORT || '3306',
     dialectOptions: {
-      authPlugins: {
-        mysql_native_password: () => () => {
-          const password = process.env.DB_PASSWORD || 'stevenadams';
-          return Buffer.from(`${password}\0`);
-        },
-      },
+      authPlugin: 'mysql_native_password',
+      connectTimeout: 60000,
+      acquireTimeout: 60000,
+      timeout: 60000,
+      ssl: false,
+      insecureAuth: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  },
+  production: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'stevenadams',
+    database: process.env.DB_DATABASE || 'nbavizdemo',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: process.env.DB_DIALECT || 'mysql',
+    port: process.env.DB_PORT || '3306',
+    dialectOptions: {
+      authPlugin: 'mysql_native_password',
+      connectTimeout: 60000,
+      acquireTimeout: 60000,
+      timeout: 60000,
+      ssl: false,
+      insecureAuth: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
   },
 };
